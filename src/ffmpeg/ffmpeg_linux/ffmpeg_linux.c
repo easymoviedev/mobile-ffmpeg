b952ac1794eaef6c8f2249ef236b02e72856627b
lsegretin <louis.segretin@easy.movie>
2020-10-09 17:17:21 +0200

    Renaming file

--- src/ffmpeg/ffmpeg_linux/ffmpeg_linux.c
+++
@@ -1,6 +0,0 @@
-#include "libavutil/log.h"
-
-
-void mobileffmpeg_log_callback_function(void *ptr, int level, const char* format, va_list vargs){
-    av_log_default_callback(ptr, level, format, vargs);
-}
